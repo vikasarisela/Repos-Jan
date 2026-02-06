4e92fb0f58ff8adb457c3e578a7e81c5dba42031
unknown <vikas.arisela@gmail.com>
2026-02-06 17:28:25 +0400

    variables practice-total

---
+++ ShellScripting-Practice/datatypes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+number1=100
+number2=200
+name=vikas
+sum=$(($number1+$number2+$name))
+echo "total is ${sum}"
+
+#prints only  300  because name can't be added  name will be consider as 0  
+
+#size = 3 index = 2
+Leaders=("trump" "trudo" "modi")
+echo "all leaders ${Leaders[@]}"
+echo "all leaders ${Leaders[1]}"
+echo "all leaders ${Leaders[2]}"
