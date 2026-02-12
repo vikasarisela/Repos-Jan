8095440767c25b6f6dc8d66d6ebcedb311ad1c05
unknown <vikas.arisela@gmail.com>
2026-02-12 07:48:13 +0400

    roboshop

--- Shell-Roboshop/roboshop.sh
+++ Shell-Roboshop/roboshop.sh
@@ -5,22 +5,22 @@ SECURITY_GROUP=sg-05070ca66b56218cd
 
 for instance in $@
 do 
-Instance_Id=$(aws ec2 run-instances \
---image-id ami-0220d79f3f480ecf5 \
---count 1 \
---instance-type t2.micro \
---security-group-ids sg-05070ca66b56218cd \
---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
---query 'Instances[0].InstanceId' \
---output text)
+  Instance_Id=$(aws ec2 run-instances \
+  --image-id ami-0220d79f3f480ecf5 \
+  --count 1 \
+  --instance-type t2.micro \
+  --security-group-ids sg-05070ca66b56218cd \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+  --query 'Instances[0].InstanceId' \
+  --output text)
 
 if [ $instance != "frontend" ]; then
-IP=$(aws ec2 describe-instances \
+    IP=$(aws ec2 describe-instances \
     --instance-ids $Instance_Id \
     --query 'Reservations[0].Instances[0].PrivateIpAddress' \
     --output text)
-else 
-IP=$(aws ec2 describe-instances \
+   else 
+   IP=$(aws ec2 describe-instances \
     --instance-ids $Instance_Id \
     --query 'Reservations[0].Instances[0].PublicIpAddress' \
     --output text)
