5a1383dd1e434100c2b65822fdc7ec3037be376a
unknown <vikas.arisela@gmail.com>
2026-02-12 07:40:39 +0400

    roboshop

--- Shell-Roboshop/roboshop.sh
+++ Shell-Roboshop/roboshop.sh
@@ -14,17 +14,18 @@ Instance_Id=$(aws ec2 run-instances \
 --query 'Instances[0].InstanceId' \
 --output text)
 
- if [$instance != "frontend"]; then
-aws ec2 describe-instances \
+if [ $instance != "frontend" ]; then
+IP=$(aws ec2 describe-instances \
     --instance-ids $Instance_Id \
     --query 'Reservations[0].Instances[0].PrivateIpAddress' \
-    --output text
+    --output text)
 else 
-aws ec2 describe-instances \
+IP=$(aws ec2 describe-instances \
     --instance-ids $Instance_Id \
     --query 'Reservations[0].Instances[0].PublicIpAddress' \
-    --output text
+    --output text)
 fi
 
+echo "$IP, $instance" 
 
 done 
