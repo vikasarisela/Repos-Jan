cea5bc8a5e2fd23c6e7b783de79c51ff920f2c42
unknown <vikas.arisela@gmail.com>
2026-02-13 11:46:08 +0400

    roboshop

--- Shell-Roboshop/mongodb.sh
+++ Shell-Roboshop/mongodb.sh
@@ -1,51 +1,46 @@
-#bin/bash
+#!/bin/bash
 
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-# log folder creating in 
-LOG_FOLDER=/var/log/shellscripting-logs
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1)   #$0 you will get script name 
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
-mkdir -p $LOG_FOLDER
-echo "script started executed at $(date)"  | tee -a $LOG_FILE
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-USERID=$(id -u)
-if [ $USERID -ne 0 ]; then 
-  echo "Error: Please run the script with root priviliges"
-  exit 1
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-
-#Calling Function recieve inputs through args 
-validate(){
-if [ $1 -ne 0 ]; then    # 0 is success and 1-127 is failure 
-     echo -e "$2 .... $R FAILURE $N" | tee -a $LOG_FILE
-     exit 1
-  else
-    echo -e "$2 .... $G SUCCESSFUL $N.." | tee -a $LOG_FILE    
-  fi
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
 }
 
-
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-validate $? "Adding Mongo Repo"
+VALIDATE $? "Adding Mongo repo"
 
-dnf install mongodb-org -y  &>>$LOG_FILE
-validate $? "Installing MongoDB"   #$1 and $2 
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB"
 
 systemctl enable mongod &>>$LOG_FILE
-validate $? "Enabled MongoDB" 
-systemctl start mongod &>>$LOG_FILE
-validate $? "Started MongoDB" 
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod 
+VALIDATE $? "Start MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-validate $? "Allowing remote connections.. to mongodb"
+VALIDATE $? "Allowing remote connections to MongoDB"
 
 systemctl restart mongod
-validate $? "Restarted MongoDB"
-
-
+VALIDATE $? "Restarted MongoDB"
