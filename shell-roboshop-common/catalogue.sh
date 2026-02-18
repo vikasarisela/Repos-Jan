ddcf99472cac3d00a2adbeaa2827ac0a0e3e465e
unknown <vikas.arisela@gmail.com>
2026-02-18 14:54:25 +0400

    shell roboshop common

--- shell-roboshop-common/catalogue.sh
+++ shell-roboshop-common/catalogue.sh
@@ -14,12 +14,12 @@ validate $? "Copying MongoRepo to yum repos.."
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 validate $? "Installing Mongodb client.."
 
-INDEX=$(mongosh mongodb.cloudskills.fun --quiet --eval "db.getMongo().getDBNames().includes('catalogue')")
-if [ $INDEX -le 0 ]; then 
-    mongosh --host mongodb.cloudskills.fun </app/db/master-data.js  &>>$LOG_FILE
-    validate $? "Load $app_name products"
-else 
-    echo -e "$app_name products already loaded ... $Y SKIPPING.. $N"
+INDEX=$(mongosh mongodb.cloudskills.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Load $app_name products"
+else
+    echo -e "$app_name products already loaded ... $Y SKIPPING $N"
 fi
 
 app_restart
