673c629f957ff4395afdc1d8f311891e777d16f0
unknown <vikas.arisela@gmail.com>
2026-02-20 16:46:27 +0400

    shell

---
+++ ShellScripting-Practice/17-delete-logfiles.sh
@@ -0,0 +1,31 @@
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+# log folder creating in 
+LOG_FOLDER=/var/log/shellscripting-logs
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)   #$0 you will get script name 
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOG_FOLDER
+echo "script started executed at $(date)"  | tee -a $LOG_FILE
+
+
+
+SOURCE_DIR=/home/ec2-user/app-logs
+
+if [ ! -d $SOURCE_DIR ]; then # to check source directory exist or not , if there is it is true
+    echo -e "ERROR:: $SOURCE_DIR does not exist"
+    exit 1 
+fi  
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log"  -mtime  +14)
+
+while IFS= read -r filepath 
+do
+    # Process each line here
+    echo "Deleting the file: $filepath"
+    rm -rf $filepath
+    echo "Deleted the file: $filepath"
+done <<< $FILES_TO_DELETE
---
+++ ShellScripting-Practice/18-while-loop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# count=5
+
+# echo="start count down"
+
+# while [ $count -gt 0 ]
+# do 
+#     echo "Time Left: $count"
+#     sleep 1 
+#     count=$(($count -1))
+# done
+
+while IFS= read -r filepath 
+do
+    # Process each line here
+    echo "Deleting the file: $filepath"
+done <<< 15-script-2.sh
