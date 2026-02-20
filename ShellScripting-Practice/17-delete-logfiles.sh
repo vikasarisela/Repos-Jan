d0f814f0d19823915bc7e551213216c8d50b5e14
unknown <vikas.arisela@gmail.com>
2026-02-20 17:20:03 +0400

    shell

--- ShellScripting-Practice/17-delete-logfiles.sh
+++ ShellScripting-Practice/17-delete-logfiles.sh
@@ -25,6 +25,6 @@ FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
 while IFS= read -r filepath
 do
     echo "Deleting the file: $filepath"
-    #rm -rf $filepath
+    rm -rf $filepath
     echo "Deleted the file: $filepath"
 done <<< $FILES_TO_DELETE
