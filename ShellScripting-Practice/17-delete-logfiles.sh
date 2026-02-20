fc284ba30be103aa4147c620d0a6b21e113a4fed
unknown <vikas.arisela@gmail.com>
2026-02-20 17:41:06 +0400

    shell

--- ShellScripting-Practice/17-delete-logfiles.sh
+++ ShellScripting-Practice/17-delete-logfiles.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-set -euo pipefail
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
 
-LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+SOURCE_DIR=/home/ec2-user/app-logs
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
-
-SOURCE_DIR=/home/ec2-user/app-logss
-
+#checks the directory exists or not
  if [ ! -d $SOURCE_DIR ]; then
     echo -e "ERROR:: $SOURCE_DIR does not exist"
     exit 1
